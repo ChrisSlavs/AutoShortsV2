6d2577bbb8156bc76a99f5a920e2864c083cc4d7
Chris Slaveski <119473224+ChrisSlavs@users.noreply.github.com>
2024-02-01 00:34:20 -0500

    Add regex and api information

---
+++ CONSTANTS.py
@@ -0,0 +1,19 @@
+import re
+
+regex = {"EMOJI": re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])'),
+         "ALPHANUMERIC": re.compile(r'\W+')}
+
+elevenlabs_data = {"API_KEY": "",
+                   "url"    : "https://api.elevenlabs.io/v1/text-to-speech/",
+                   "voices" : {"Alex": "hKULXlJp90RYPLVAaOJI"}, 
+                   "headers" : {"Accept":"audio/mpeg",
+                              "Content-Type":"application/json",
+                              "xi-api-key":""},
+                    "data"  : {"model_id": "eleven_monolingual_v1",
+                               "text": "",
+                               "voice_settings": {
+                               "stability": 0.82,
+                               "similarity_boost": 0.75,
+                               "style": 0.0,
+                               "speaker_boost": False}},}
+
